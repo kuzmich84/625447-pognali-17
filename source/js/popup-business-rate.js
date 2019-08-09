5045bc72078d75d664f3a874107ea1a41e8dd342
Dmitry Kuchukov <kudim84@yandex.ru>
2019-08-09 20:12:05 +0300

    add *.less

---
+++ source/js/popup-business-rate.js
@@ -0,0 +1,23 @@
+let modalBusinessRate = document.querySelector('.business-rate__popup');
+let open = document.querySelector('.rate__link--business');
+let close =  document.querySelector('.business-rate__button');
+
+
+open.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalBusinessRate.classList.add('business-rate__popup--show');
+});
+
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalBusinessRate.classList.remove('business-rate__popup--show');
+});
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (modalBusinessRate.classList.contains('business-rate__popup--show')) {
+      evt.preventDefault();
+      modalBusinessRate.classList.remove('business-rate__popup--show');
+    }
+  }
+});
